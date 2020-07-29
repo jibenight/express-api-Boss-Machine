6560de0283f60a7d633276b663bcb4e4f38951e1
Jean <nguyen.jean@me.com>
2020-07-29 11:59:00 +0200

    add get an array of all minions.

--- server/minions.js
+++ server/minions.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const minions = express.Router();
 
+const {
+  createMeeting,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+  deleteAllFromDatabase,
+} = require('./db');
+
 minions.get('/minions', (req, res) => {
-  res.send('Hello Minions :)');
+  const showMinions = getAllFromDatabase('minions');
+  res.send(showMinions);
 });
 
 module.exports = minions;
